dfbfb66f3a010d8e929c57b8c858e50b2644ef49
criddanai <criddanai@gmail.com>
2024-12-02 14:19:56 +0700

    LabSheet02

--- LabSheet02/src/Lab202.java
+++ LabSheet02/src/Lab202.java
@@ -5,13 +5,24 @@ public class Lab202 {
 	public static void main(String[] args) {
 		
 		Scanner input = new Scanner(System.in);
+		System.out.print("Input number  : ");
+		int number = input.nextInt();
+		
+		int tempNum = Integer.MIN_VALUE;
 		
 		// using break statement
 		
+		while(true) {
 		
-		System.out.print("Input number  : ");
-		int number = input.nextInt();
-
+			if (tempNum > number) {
+				System.out.println();
+				System.out.println("BYE BYE");
+				break;
+			}
+			
+			tempNum = number;
+			System.out.print("Input number  : ");
+			number = input.nextInt();
+		}
 	}
-
 }
