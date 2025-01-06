939f43c478c9245bd6f711170677fed195770125
criddanai <criddanai@gmail.com>
2025-01-07 03:05:08 +0700

    LabSheet05[Practice]

--- LabSheet05/src/Lab504.java
+++ LabSheet05/src/Lab504.java
@@ -1,22 +1,30 @@
 import java.util.*;
 
 public class Lab504 {
+    public static void main(String[] args) {
 
-	public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double[] score = new double[5];
+        double avg = 0;
+        int index = 0;
 
-		Scanner input = new Scanner(System.in);
-		double[] score = new double[5];
-		double avg = 0;
-		double sum = 0;
-		
-		for (int i = 0; i < score.length; i++) {
-			System.out.print("Input score of student " + (i + 1) + " : ");
-			score[i] = input.nextDouble();
-			avg += score[i];
-		}
-		avg = avg/ (score.length);
-		System.out.printf();
-		
-		input.close();
-	}
+        for (int i = 0; i < score.length; i++) {
+            System.out.print("Input score of student " + (i + 1) + " : ");
+            score[i] = input.nextDouble();
+            avg += score[i];
+        }
+        System.out.println();
+        avg /= score.length;
+        System.out.printf("Average of %s student is %.2f%n", score.length, avg);
+
+        for (double _score : score) {
+            index++;
+
+            if (_score > avg) {
+                System.out.printf("> Student %s (%.2f)%n", index, _score);
+            }
+        }
+
+        input.close();
+    }
 }
--- LabSheet05/src/Lab508.java
+++ LabSheet05/src/Lab508.java
@@ -1,9 +1,37 @@
+import javax.swing.*;
 
 public class Lab508 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
+        int[] nums = { 25, 78, 41, 22, 36, 85, 37 };
 
-	}
+        int data = Integer.parseInt(JOptionPane.showInputDialog("Input index of array:"));
 
+        while (!checkIndex(nums, data)) {
+            data = Integer.parseInt(JOptionPane.showInputDialog("Input index of array, again:"));
+        }
+
+        int currentData = currentData(nums, data);
+            JOptionPane.showMessageDialog(null,
+                    "Current data, nums[" + data + "] is " + currentData
+                            + "\n" + (data != 0 ? "Previous data, nums[" + (data - 1) + "] is " + prevData(nums, data) : "No previous data")
+                            + "\n" + (data != nums.length ? "Next data, nums[" + (data + 1) + "] is " + nextData(nums, data) : "No next data"));
+
+    } // end of main() method
+
+    public static boolean checkIndex(int[] nums, int index) {
+        return (index >= nums.length ? false : true);
+    } // end of checkIndex() method
+
+    public static int currentData(int[] nums, int index) {
+        return nums[index];
+    } // end of currentData() method
+
+    public static int prevData(int[] nums, int index) {
+        return nums[index - 1];
+    } // end of prevData() method
+
+    public static int nextData(int[] nums, int index) {
+        return nums[index + 1];
+    } // end of nextData() method
 }
